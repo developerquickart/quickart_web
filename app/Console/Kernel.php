b2c25b56b1ab0534aa455cc81d466360f45826a2
developerquickart <developer@quickart.ae>
2026-03-26 10:43:53 +0530

    fix 3 raliway

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,14 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Console\Commands\GenerateSitemap; 
-
 
 class Kernel extends ConsoleKernel
 {
-     protected $commands = [
-        GenerateSitemap::class,
-    ];
     /**
      * Define the application's command schedule.
      *
