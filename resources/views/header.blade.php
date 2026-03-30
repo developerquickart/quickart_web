c1c22ca961c9b9dcdf05ed8cfef0d4367f9f372c
developerquickart <developer@quickart.ae>
2026-03-30 18:31:01 +0530

    clock icon removed

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -754,11 +754,6 @@
                     <div class="qk-delivery-eta" role="status" aria-live="polite" title="Estimated delivery time" data-delivery-eta-root>
                         <span class="qk-delivery-eta__glow" aria-hidden="true"></span>
                         <div class="qk-delivery-eta__left">
-                            <div class="qk-delivery-eta__icon" aria-hidden="true">
-                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13H11v6l5.2 3.2.8-1.3-4.5-2.7V7z" fill="currentColor"/>
-                                </svg>
-                            </div>
                             <div class="qk-delivery-eta__body">
                                 <span class="qk-delivery-eta__label">Delivery in</span>
                                 <div class="qk-delivery-eta__headline">
