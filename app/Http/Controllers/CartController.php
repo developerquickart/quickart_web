38565ea6c0c63fa66c244d1192dda9a5acd7a144
developerquickart <developer@quickart.ae>
2026-03-31 12:53:07 +0530

    limit for quick pay removed

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -874,6 +874,10 @@ function checkSelectedTimeslotCashback($data) {
     
      public function dailyOrderSuccess(Request $request)
     {
+        // Allow longer processing time for payment success callback
+        @set_time_limit(0);
+        @ini_set('max_execution_time', '0');
+
         // echo "<pre>";
         // print_r($request->all());
         // die();
